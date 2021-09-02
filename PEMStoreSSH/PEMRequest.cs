6149b9f98359e5f5cef5cac370d30cdc3995a4c8
Matthew Dobrowsky <matthew.dobrowsky@keyfactor.com>
2021-09-02 14:09:59 -0400

    add missing license headers

--- PEMStoreSSH/PEMRequest.cs
+++ PEMStoreSSH/PEMRequest.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿// Copyright 2021 Keyfactor
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+// and limitations under the License.
+
+using System;
 using System.Security.Cryptography.X509Certificates;
 
 using Newtonsoft.Json;
@@ -12,7 +19,7 @@ namespace PEMStoreSSH
         private class PEMRequest
         {
 
-            private string _pfxPassword = String.Empty;
+            private string _pfxPassword = string.Empty;
             public bool IsPfxFile { get; }
             public X509Certificate2 Certificate { get; }
             public string StorePath { get; }
@@ -27,7 +34,7 @@ namespace PEMStoreSSH
                 StorePassword = config.Store.StorePassword;
                 _pfxPassword = config.Job.PfxPassword;
 
-                IsPfxFile = String.IsNullOrEmpty(config.Job.PfxPassword) ? false : true;
+                IsPfxFile = !string.IsNullOrEmpty(config.Job.PfxPassword);
 
                 if (IsPfxFile)
                 {
@@ -39,8 +46,8 @@ namespace PEMStoreSSH
                 }
 
                 dynamic properties = JsonConvert.DeserializeObject(config.Store.Properties.ToString());
-                HasSeparatePrivateKey = properties.separatePrivateKey == null || string.IsNullOrEmpty(properties.separatePrivateKey.Value) ? false : Boolean.Parse(properties.separatePrivateKey.Value);
-                KeyPath= HasSeparatePrivateKey ? (properties.pathToPrivateKey == null || string.IsNullOrEmpty(properties.pathToPrivateKey.Value) ? null : properties.pathToPrivateKey.Value) : string.Empty;
+                HasSeparatePrivateKey = properties.separatePrivateKey == null || string.IsNullOrEmpty(properties.separatePrivateKey.Value) ? false : bool.Parse(properties.separatePrivateKey.Value);
+                KeyPath = HasSeparatePrivateKey ? (properties.pathToPrivateKey == null || string.IsNullOrEmpty(properties.pathToPrivateKey.Value) ? null : properties.pathToPrivateKey.Value) : string.Empty;
 
             }   
         }
